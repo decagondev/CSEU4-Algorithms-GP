c941e13e49d137f12219c7f075464bcff2ed9a7a
Tom <tomtarpeydev@gmail.com>
2020-04-15 09:36:15 +0100

    day1 algo what is it?

--- algorithm.py
+++ algorithm.py
@@ -1,1 +1,22 @@
 # add 1
+"""
+1. Take input from caller (num): an integer
+- if the input can not be cast to int then throw an exception and gracefully exit function
+2. set output to (num plus 1)
+3. return output to caller
+"""
+
+def add_one(num):
+    try:
+        output = int(num) + 1
+        return output
+    except:
+        # print("Value Error input int")
+        return f"ERROR: {num} is not an integer: Please input an integer"
+
+# test
+print(add_one(4)) # => 5
+print(add_one(15)) # => 16
+print(add_one(40)) # => 41
+name = add_one("Dave") # ?
+print(name) # => 21
